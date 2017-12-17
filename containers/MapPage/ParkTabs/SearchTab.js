ca5e732eaa89a5ec77565538dd728e0db78d45c9
Yaroslav R <rostanets@gmail.com>
2017-12-18 01:04:58 +0200

    added checkbox

--- containers/MapPage/ParkTabs/SearchTab.js
+++ containers/MapPage/ParkTabs/SearchTab.js
@@ -1,18 +1,64 @@
 /**
  * Created by Yaroslav on 23.09.2017.
  */
-
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { createIconSetFromFontello } from 'react-native-vector-icons';
 import fontelloConfig from '../../../src/config.json';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
+import {MONFRY, SAT, SUN, FROM, TO} from '../../../constants/Filter'
+
+import CheckboxField from 'react-native-checkbox-field';
+
 const CustIcon = createIconSetFromFontello(fontelloConfig);
 
 class SearchTab extends Component {
 
+    state = {
+        MONFRY: true,
+        SAT: false,
+        SUN: true,
+        filterFrom: "14-00",
+        filterTo: "16-00",
+        filterTimeFrom: "30min",
+        filterTimeTo: "12h",
+        sliderValues: [1, 6]
+    };
+
+    selectCheckbox = (selectedDays) => {
+
+        switch(selectedDays) {
+            case MONFRY:
+                this.setState({
+                    MONFRY: !this.state.MONFRY
+                });
+                break;
+            case SAT:
+                this.setState({
+                    SAT: !this.state.SAT
+                });
+                break;
+            case SUN:
+                this.setState({
+                    SUN: !this.state.SUN
+                });
+                break;
+            default :
+                console.log("Error, Ошибка установки дней");
+        }
+
+        this.setState({
+            selectedDays: false
+        })
+
+    };
+
     render(){
+
+        const monFry = this.state.MONFRY;
+        const sat = this.state.SAT;
+        const sun = this.state.SUN;
 
         return (
             <View style={styles.searchTab}>
@@ -24,6 +70,42 @@ class SearchTab extends Component {
                         <Text style={styles.topDescr}>
                             Day:
                         </Text>
+                        <View style={styles.checkbox}>
+                            <View style={{...styles.oneCheck,marginLeft: -20, width: "auto"}}>
+                                <CheckboxField
+                                    onSelect={this.selectCheckbox.bind(this,MONFRY)}
+                                    selected={monFry}
+                                    labelSide="right"
+                                    label="Mon-Fry"
+                                    labelStyle={styles.labelStyle}
+                                >
+                                    <Icon name="check" color="#fff" />
+                                </CheckboxField>
+                            </View>
+                            <View style={{...styles.oneCheck, width: "auto"}}>
+                                <CheckboxField
+                                    onSelect={this.selectCheckbox.bind(this, SAT)}
+                                    selected={sat}
+                                    labelSide="right"
+                                    label="Sat"
+                                    labelStyle={styles.labelStyle}
+                                >
+                                    <Icon name="check" color="#fff" />
+                                </CheckboxField>
+                            </View>
+                            <View style={{...styles.oneCheck, width: "auto"}}>
+                                <CheckboxField
+                                    onSelect={this.selectCheckbox.bind(this, SUN)}
+                                    selected={sun}
+                                    labelSide="right"
+                                    label="Sun"
+                                    labelStyle={styles.labelStyle}
+                                >
+                                    <Icon name="check" color="#fff" />
+                                </CheckboxField>
+                            </View>
+
+                        </View>
                     </View>
                 </View>
                 <View style={styles.oneRow}>
@@ -76,8 +158,8 @@ const styles = {
         flexDirection: "row",
         paddingLeft: 8,
         paddingRight: 8,
-        paddingTop: 14,
-        paddingBottom: 14
+        paddingTop: 10,
+        paddingBottom: 10
     },
     iconWrap: {
         height: "100%",
@@ -97,7 +179,26 @@ const styles = {
     },
     topDescr: {
         color: "#5093DF",
-        fontSize: 16
+        fontSize: 15
+    },
+    checkbox: {
+        display: "flex",
+        flexDirection: "row",
+        width: "100%"
+    },
+    oneCheck: {
+        width: 120,
+        marginLeft: -20,
+        flexGrow: 1
+    },
+    labelStyle: {
+        fontSize: 14,
+        color: "#6F7071",
+        marginLeft: 8
+    },
+    checkboxStyle: {
+        width: 24,
+        height: 24
     }
 };
 
