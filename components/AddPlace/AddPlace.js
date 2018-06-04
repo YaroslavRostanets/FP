906282eb3b4451871bb8e3d2f331348a1fd19d5e
Yaroslav R <rostanets@gmail.com>
2018-06-04 22:18:47 +0300

    fix

---
+++ components/AddPlace/AddPlace.js
@@ -0,0 +1,32 @@
+/**
+ * Created by Yaroslav on 04.06.2018.
+ */
+import React, { Component } from 'react';
+import { View, Text, Button } from 'react-native';
+
+
+
+class AddPlace extends Component {
+
+
+
+
+    render() {
+
+        return (
+            <View style={styles.mapPage}>
+                <Text>TEST</Text>
+            </View>
+        )
+    }
+}
+
+const styles = {
+    preview: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'center'
+    }
+};
+
+export default AddPlace
--- constants/routes.js
+++ constants/routes.js
@@ -6,5 +6,6 @@ export const routes = [
     {title: 'ParkDetail', index: 1, parentProps: this.props, animationType: "11"},
     {title: 'Info', index: 2, parentProps: this.props, animationType: "11"},
     {title: 'Options', index: 3, parentProps: this.props, animationType: "11"},
-    {title: 'PreLoader', index: 4, parentProps: this.props, animationType: "11"}
+    {title: 'PreLoader', index: 4, parentProps: this.props, animationType: "11"},
+    {title: 'AddPlace', index: 5, parentProps: this.props, animationType: "11"},
 ];
